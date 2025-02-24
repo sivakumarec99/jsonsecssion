01595c4a6320348bb3e6d7ff632f07d273cdd323
SathishKumar <sathishkumar.thulasiraman@jasmin-infotech.com>
2025-02-24 16:58:30 +0530

    API integration with sample URL . Using Swfit UI code

---
+++ JasonSession/JasonSession/Products/Model/ProductNewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProductNewModel.swift
+//  JasonSession
+//
+//  Created by JIDTP1408 on 24/02/25.
+//
+
+import Foundation
---
+++ JasonSession/JasonSession/Products/data/Product.swift
@@ -0,0 +1,8 @@
+//
+//  Product.swift
+//  JasonSession
+//
+//  Created by JIDTP1408 on 24/02/25.
+//
+
+import Foundation
